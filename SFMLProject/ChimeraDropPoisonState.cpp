2ec7effbb83907c88b7d1f8d8e81d421ba32ab20
parksoungmin <psmin5812@gmail.com>
2024-11-27 14:18:14 +0900

    키메라 클래스, 상태 구조 추가

---
+++ SFMLProject/ChimeraDropPoisonState.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "ChimeraDropPoisonState.h"
+
+ChimeraDropPoisonState::ChimeraDropPoisonState(ChimeraFSM* fsm)
+
+{
+}
+
+ChimeraDropPoisonState::~ChimeraDropPoisonState()
+{
+}
+
+void ChimeraDropPoisonState::Awake()
+{
+}
+
+void ChimeraDropPoisonState::Start()
+{
+}
+
+void ChimeraDropPoisonState::Enter()
+{
+}
+
+void ChimeraDropPoisonState::Exit()
+{
+}
+
+void ChimeraDropPoisonState::Update(float deltaTime)
+{
+}
+
+void ChimeraDropPoisonState::FixedUpdate(float fixedDeltaTime)
+{
+}
+
+void ChimeraDropPoisonState::LateUpdate(float deltaTime)
+{
+}
---
+++ SFMLProject/ChimeraNomalAttackState.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "ChimeraNomalAttackState.h"
+
+ChimeraNomalAttackState::ChimeraNomalAttackState(ChimeraFSM* fsm)
+	:ChimeraBaseState()
+{
+}
+
+ChimeraNomalAttackState::~ChimeraNomalAttackState()
+{
+}
+
+void ChimeraNomalAttackState::Awake()
+{
+}
+
+void ChimeraNomalAttackState::Start()
+{
+}
+
+void ChimeraNomalAttackState::Enter()
+{
+}
+
+void ChimeraNomalAttackState::Exit()
+{
+}
+
+void ChimeraNomalAttackState::Update(float deltaTime)
+{
+}
+
+void ChimeraNomalAttackState::FixedUpdate(float fixedDeltaTime)
+{
+}
+
+void ChimeraNomalAttackState::LateUpdate(float deltaTime)
+{
+}
